46d5f567f6d6f845fb930806b03abbecbd5f8d0f
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2024-06-27 20:10:44 +0900

    done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,20 @@
+require 'open-uri'
+
 class FlatsController < ApplicationController
-  def index
+  before_action :get_flats
+
+  def index; end
+
+  def show
+    # Get the id of the flat the user wants
+    # Look inside the JSON and find the hash with the flat info
+    @flat = @flats.find { |flat| flat['id'] == params[:id].to_i }
+  end
+
+  private
+
+  def get_flats
+    url = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json'
+    @flats = JSON.parse(URI.open(url).read)
   end
 end
--- test/system/flats_test.rb
+++ test/system/flats_test.rb
@@ -5,5 +5,11 @@ class FlatsTest < ApplicationSystemTestCase
     visit '/'
 
     assert_selector 'h1', text: 'Flats'
+    assert_selector 'li', text: 'Trendy Apt in Buttes Montmartre'
+  end
+
+  test 'visiting flat show page' do
+    visit '/flats/145'
+    assert_selector 'h1', text: 'Charm at the Steps of the Sacre Coeur/Montmartre'
   end
 end
